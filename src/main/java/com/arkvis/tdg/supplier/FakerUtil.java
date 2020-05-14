66584ee534673db085d4709d04658803b1114bc9
Miguel Hernandez <phyzix5761@gmail.com>
2020-05-13 20:35:10 -0400

    Clean up

--- src/main/java/com/arkvis/tdg/supplier/FakerUtil.java
+++ src/main/java/com/arkvis/tdg/supplier/FakerUtil.java
@@ -1,22 +1,8 @@
 package com.arkvis.tdg.supplier;
 
-import com.arkvis.tdg.generator.Csv;
-import com.arkvis.tdg.generator.CsvBuilder;
 import com.github.javafaker.Faker;
 
-import java.util.Random;
-
 class FakerUtil {
-
-    public static void main(String[] args) {
-        Csv csv = new CsvBuilder(5)
-                .column("FIRST_NAME", new FirstNameSupplier())
-                .column("LAST_NAME", new LastNameSupplier())
-                .column("CUSTOM", () -> String.valueOf(new Random().nextFloat()))
-                .build();
-
-        System.out.println(csv.toString());
-    }
 
     private static Faker faker;
 
