04c319a11c8c531af869cafbf0174e20e4f3747b
Miguel Hernandez <phyzix5761@gmail.com>
2020-05-17 11:00:14 -0400

    Consolidated name tests

--- src/test/java/com/arkvis/tdg/supplier/NameSupplierTest.java
+++ src/test/java/com/arkvis/tdg/supplier/NameSupplierTest.java
@@ -7,7 +7,13 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class LastNameSupplierTest {
+public class NameSupplierTest {
+
+    @Test
+    public void should_returnFirstName_when_supplying() {
+        String name = new FirstNameSupplier().get();
+        assertNotNull(name);
+    }
 
     @Test
     public void should_returnLastName_when_supplying() {
@@ -16,10 +22,10 @@ public class LastNameSupplierTest {
     }
 
     @Test
-    public void should_createCsvWithLastName_when_creatingCsv() {
+    public void should_createCsvWithName_when_creatingCsv() {
         int rowCount = 10;
-
         Csv csv = new CsvBuilder(rowCount)
+                .column("FIRST_NAME", new FirstNameSupplier())
                 .column("LAST_NAME", new LastNameSupplier())
                 .build();
 
@@ -28,5 +34,6 @@ public class LastNameSupplierTest {
 
         assertEquals(rowCount, csv.getRows().size());
         csv.getRows().forEach(row -> assertNotNull(row.get(0)));
+        csv.getRows().forEach(row -> assertNotNull(row.get(1)));
     }
 }
